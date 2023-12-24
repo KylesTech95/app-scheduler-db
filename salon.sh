37b7284346b5f16431cfac345221da6afdfd6c6a
kylestech95 <kylestech95@outlook.com>
2023-12-24 18:55:17 -0500

    forking structure of salon

--- salon.sh
+++ salon.sh
@@ -1,5 +1,34 @@
 #! /bin/bash
 PSQL="psql --username=postgres --dbname=salon --tuples-only -c "
+#clear tables & reset sequence
+echo $($PSQL "truncate appointments,customers;alter sequence customers_customer_id_seq restart with 1;alter sequence appointments_appointment_id_seq restart with 1;")
+
+# enter phone number
+ENTER_PHONE(){
+  echo -e "\nWhat is your phone number? (1234567890 or 12343452345)."
+  sleep 1
+  read CUSTOMER_PHONE
+  if [[ ! $CUSTOMER_PHONE =~ ^(1)?[0-9]{3}[0-9]{3}[0-9]{4}$ ]]
+    then
+    ENTER_PHONE
+    else
+    ENTER_NAME
+  fi
+}
+# Enter Name
+ENTER_NAME(){
+  # What is your name?
+    echo -e "\nWhat is your name?"
+    sleep 1
+      read CUSTOMER_NAME
+      if [[ ! $CUSTOMER_NAME =~ ^[a-zA-Z]+$ ]]
+        then
+          # PLACE YOUR STATEMENT
+          ENTER_NAME
+        else
+          ENTER_SERVICE_TIME
+      fi
+}
 # re-enter service number
 CHOOSE_AGAIN(){
   # display services again
@@ -29,81 +58,6 @@ CHOOSE_AGAIN(){
 IDENTIFY_SERVICE_CHOSEN(){
   echo -e "Great, you chose $1."
 }
-# re-enter name
-NAME_AGAIN(){
-    echo -e "\nWhat is your name? "
-  sleep 1
-      read CUSTOMER_NAME
-  if [[ ! $CUSTOMER_NAME =~ ^[a-zA-Z]+$ ]]
-  then
-  NAME_AGAIN
-  else
-    echo -e "\nYour name is $CUSTOMER_NAME"
-    sleep 1
-    SERVICE_AGAIN
-  fi
-}
-# re-enter phone number
-PHONE_AGAIN(){
-  echo -e "\nWhat is your phone number? (format: 012-345-6789) "
-  read CUSTOMER_PHONE
-  if [[ ! $CUSTOMER_PHONE =~ ^(1-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]]
-  then
-  PHONE_AGAIN
-  else
-    echo -e "\nYour phone number is $CUSTOMER_PHONE"
-    sleep 1
-    NAME_AGAIN
-  fi
-}
-# re-enter service time
-SERVICE_AGAIN(){
-        echo -e "\nWhen would you like to come? (format: 1:45 or 01:45)"
-        read SERVICE_TIME
-        if [[ ! $SERVICE_TIME =~ ^[0-9]{1,2}:([0]{2}|[0-9]{2})$ ]]
-        then
-          SERVICE_AGAIN
-        else
-          echo -e "\nYour Service Appointed time is $SERVICE_TIME" | sed -E 's/^([0-9]{1}):/0\1:/'
-            sleep 1
-        fi
-}
-#What is the customer's information
-CUSTOMER_INFORMATION(){
-  # What is your phone number?
-  echo -e "\nWhat is your phone number? (format: 012-345-6789)"
-  sleep 1
-  read CUSTOMER_PHONE
-  if [[ ! $CUSTOMER_PHONE =~ ^(1-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]]
-  then
-  #invalid phone number
-  echo -e "\nThis is not a vaild phone number. Try again."
-  PHONE_AGAIN
-  else
-  # What is your name?
-  echo -e "\nWhat is your name?"
-    sleep 1
-    read CUSTOMER_NAME
-      if [[ ! $CUSTOMER_NAME =~ ^[a-zA-Z]+$ ]]
-      then
-        NAME_AGAIN
-      else
-        # What is your service appointment TIME?
-      echo -e "\nWhen would you like to come? (format: 1:45 or 01:45)"
-        sleep 1
-        read SERVICE_TIME
-        if [[ ! $SERVICE_TIME =~ ^[0-9]{1,2}:([0]{2}|[0-9]{2})$ ]]
-        then
-        echo -e "\nChoose a valid time from our format (format: 1:45 or 01:45)"
-          SERVICE_AGAIN
-        else
-          #enter time appointed
-          echo -e "\nYour Service Appointed time is $SERVICE_TIME" | sed -E 's/([0-9]{1}):/0\1:/g'
-        fi
-      fi
-  fi
-  
-}
 WELCOME(){
   echo -e "\n ~~~~~ MY SALON ~~~~~ \n"
     if [[ $1 ]]
@@ -129,12 +83,19 @@ WELCOME(){
           #user chooses a service.
           CHOOSE_AGAIN
         else
-      NAME=$($PSQL "select name from services where service_id=$SERVICE_ID_SELECTED");
-      IDENTIFY_SERVICE_CHOSEN $NAME
+      SERVICE_NAME=$($PSQL "select name from services where service_id=$SERVICE_ID_SELECTED");
+      IDENTIFY_SERVICE_CHOSEN $SERVICE_NAME
       fi
       sleep 1
       CUSTOMER_INFORMATION
     fi
+  
+}
+
+#What is the customer's information (serviceName)
+CUSTOMER_INFORMATION(){
+  ENTER_PHONE
+  ENTER_NAME
   
 }
 
